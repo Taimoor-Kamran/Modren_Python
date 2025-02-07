c8520364c0d06320e81e5c79e3c6a9f2d2e3d273
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-02-07 11:52:47 +0500

    added third commit

---
+++ Class_02/Class02.py
@@ -0,0 +1,12 @@
+# How to I Generate Error in our code 
+
+# how to i run this code open terminal fire this command "python Class02.Py"
+
+# print(name) This is Error because he consider the varibale
+
+# print("name") This is Correct Sentence 
+
+# variables
+
+# name : str = "Taimoor Kamran"
+# print(name)
