63940b840a9268b7a19454935bcc37bca9347535
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-02-21 15:00:41 +0500

    added 33 commit

--- Class_05(list_tuple_for_loop)/.ipynb_checkpoints/Class05-checkpoint.py
+++
@@ -1,6 +0,0 @@
-data_base : list[tuple[str, str]] = [("Taimoor", '123'),
-                           ("Sir Zia", "345"),
-                           ("Sir Ameen Alam", "978")]
-
-for row in data_base:
-    print(row)
