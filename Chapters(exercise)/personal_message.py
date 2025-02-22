8073344727066bedaa59b30b9ff1a673a1824a63
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-02-22 12:48:26 +0500

    added 40 commit

--- Chapters(exercise)/personal_message.py
+++ Chapters(exercise)/personal_message.py
@@ -4,15 +4,15 @@
 
 name : str = "Eric"
 
-print(f"Hello {name}, would you like to learn some Python today?")
+# print(f"Hello {name}, would you like to learn some Python today?")
 
 # 2-4. Name Cases: Use a variable to represent a person’s name, and then print
 # that person’s name in lowercase, uppercase, and title case.
 
 person_name: str = "dr apj abdul kalam"
-print(person_name.upper())
-print(person_name.title())
-print(person_name.lower())
+# print(person_name.upper())
+# print(person_name.title())
+# print(person_name.lower())
 
 # 2-5. Famous Quote: Find a quote from a famous person you admire. Print the
 # quote and the name of its author. Your output should look something like the
@@ -23,7 +23,7 @@ print(person_name.lower())
 author:str = "Albert Einstein"
 quote: str = "A person who never made a mistake never"
 
-print(f"{author} once said, {quote}")
+# print(f"{author} once said, {quote}")
 
 
 # 2-6. Famous Quote 2: Repeat Exercise 2-5, but this time, represent the famous
@@ -35,4 +35,20 @@ famous_person: str = "Albert Einstein"
 
 message: str = f"{famous_person} once said, A person who never made a mistake never tired anything new."
 
-print(message)
+# print(message)
+
+# 2-7. Stripping Names: Use a variable to represent a person’s name, and
+# include some whitespace characters at the beginning and end of the name.
+# Make sure you use each character combination, "\t" and "\n", at least once.
+# Print the name once, so the whitespace around the name is displayed.
+# Then print the name using each of the three stripping functions, lstrip(),
+# rstrip(), and strip().
+
+# famous_person1 : str = "\t  Albert Einstein   \n"
+
+# print(repr(famous_person1))
+
+# print(repr(famous_person1.lstrip())) 
+# print(repr(famous_person1.rstrip()))
+# print(repr(famous_person1.strip()))
+
