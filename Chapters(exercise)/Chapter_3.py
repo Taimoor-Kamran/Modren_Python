85bd642bfd95d6b0801b9baeae25cb02a53902c2
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-02-23 13:10:30 +0500

    added 4 commit

---
+++ Chapters(exercise)/Chapter_3.py
@@ -0,0 +1,13 @@
+# Try these short programs to get some firsthand experience with Python’s lists.
+# You might want to create a new folder for each chapter’s exercises, to keep
+# them organized.
+# 3-1. Names: Store the names of a few of your friends in a list called names. Print
+# each person’s name by accessing each element in the list, one at a time.
+
+names: list[str] = ["Yahya", "AbdulRehman", "Ahmed", "Ali", "Ghasiq"]
+
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
