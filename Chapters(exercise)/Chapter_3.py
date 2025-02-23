8928394c4f68243c86b2e3228b9adeb2aa7bf865
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-02-23 13:22:37 +0500

    added 45 commit

--- Chapters(exercise)/Chapter_3.py
+++ Chapters(exercise)/Chapter_3.py
@@ -11,3 +11,25 @@ print(names[1])
 print(names[2])
 print(names[3])
 print(names[4])
+
+# 3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just
+
+# printing each person’s name, print a message to them. The text of each mes-
+# sage should be the same, but each message should be personalized with the
+
+# person’s name.
+
+names: list[str] = ["Yahya", "AbdulRehman", "Ahmed", "ALi", "Ghasiq"]
+
+for name in names:
+    print(f"Hy {name}, I hope you are doing good")
+
+# 3-3. Your Own List: Think of your favorite mode of transportation, such as a
+# motorcycle or a car, and make a list that stores several examples. Use your list
+# to print a series of statements about these items, such as “I would like to own a
+# Honda motorcycle.”
+
+vehicles: list[str] = ["Kawaski Ninja H2r", "Mustang 1967", "Mazda Rx7", "Mark X"]
+
+for vehicle in vehicles:
+    print(f"I would like to own a {vehicle}")
