33773efda7c51bf63b7af1eda8a5bc145de69611
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-02-25 17:46:27 +0500

    added 59 commit

--- Class_06(if_elif_else)/Class_06.py
+++ Class_06(if_elif_else)/Class_06.py
@@ -24,4 +24,28 @@ print(grade)
 
 PerType = Union[float, int]
 
-percentage: list[PerType]
+percentage: list[PerType] = [88, 99.9, 50, 51, 65, 70]
+
+grades: list[str] = []
+
+for per in percentage:
+    grade : str = ""
+
+    if (per >= 0) and (per < 33):
+        grade = "Fail"
+    elif (per >= 33) and (per < 40):
+        grade = "E"
+    elif (per >= 40) and (per < 50):
+        grade = "D"
+    elif (per >= 50) and (per < 60):
+        grade = "C"
+    elif (per >= 60) and (per < 70):
+        grade = "B"
+    elif (per >= 70) and (per < 80):
+        grade = "A"        
+    elif (per >= 80) and (per <= 100):
+        grade = "A+"
+
+grades.append(grade)
+print(percentage)
+print(grade)
