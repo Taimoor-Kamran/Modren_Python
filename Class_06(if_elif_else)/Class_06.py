7fd1b7bf2e639a6dd72303cdeef9b5e88f87c913
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-02-25 14:22:18 +0500

    added 55 commit

---
+++ Class_06(if_elif_else)/Class_06.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+per : Union[int, float] = 88
+grade: Union[str, None] = None
+
+if per >= 80:
+    grade = "A+"
+elif per >= 70:
+    grade = "A"
+elif per >= 60:
+    grade = "B"
+elif per >= 50:
+    grade = "C"
+elif per >= 40:
+    grade = "D"
+elif per >= 33:
+    grade = "E"        
+else:
+    grade = "Fail"
