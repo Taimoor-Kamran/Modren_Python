43b861e344079f1110e5023210a73410ed1aba9c
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-03-07 17:38:21 +0500

    added 107 commit

--- Chapters(exercise)/Chapter_4.py
+++ Chapters(exercise)/Chapter_4.py
@@ -6,6 +6,11 @@
 # have one line of output containing a simple statement like I like pep-
 # peroni pizza.
 
+# Add a line at the end of your program, outside the for loop, that states
+# how much you like pizza. The output should consist of three or more lines
+# about the kinds of pizza you like and then an additional sentence, such as
+# I really love pizza!
+
 pizzas:list[str] = ["pepperoni", "Greek Pizza", "Cheese pizza"]
 
 print("Pizza Names:")
@@ -17,3 +22,5 @@ print("\nPizza Preferences:")
 
 for pizza in pizzas:
     print(f"I Like {pizza} pizza." )
+
+print("\nI really love pizza!")
