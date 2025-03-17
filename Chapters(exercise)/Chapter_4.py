a8bf249da5eb5b5a89925bbd50fec71e83ae04ce
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-03-17 18:14:51 +0500

    added 128 commit

--- Chapters(exercise)/Chapter_4.py
+++ Chapters(exercise)/Chapter_4.py
@@ -120,3 +120,9 @@ for num in range(1, 11):
 
 for cube in cubes:  
     print(cube)  
+
+
+# 4-9. Cube Comprehension: Use a list comprehension to generate a list of the first 10 cubes.
+
+cubes:list[int] = [num**3 for num in range(1 ,11)]
+print("Cubes",cubes)
