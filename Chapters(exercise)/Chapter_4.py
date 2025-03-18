d6cf50664b519ad6e01df5c2ab6943a87f68f3bb
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-03-18 18:23:08 +0500

    added 133 commit

--- Chapters(exercise)/Chapter_4.py
+++ Chapters(exercise)/Chapter_4.py
@@ -191,3 +191,12 @@ print("\nfirend' favorite fruits are:")
 
 for friend_food in friend_foods:
     print(friend_food)
+
+# 4-13. Buffet: A buffet-style restaurant offers only five basic foods. Think of five
+# simple foods, and store them in a tuple.
+# • Use a for loop to print each food the restaurant offers.
+# • Try to modify one of the items, and make sure that Python rejects the
+# change.
+# • The restaurant changes its menu, replacing two of the items with different
+# foods. Add a line that rewrites the tuple, and then use a for loop to print
+# each of the items on the revised menu.
