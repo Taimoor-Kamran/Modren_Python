65bf36bc2574a33bdd39162db98b11bc7f6c353a
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-03-18 18:37:35 +0500

    added 137 commit

--- Chapters(exercise)/Chapter_4.py
+++ Chapters(exercise)/Chapter_4.py
@@ -220,3 +220,7 @@ print("\nUpdated buffet menu:")
 
 for foods in buffet_style:
     print(foods)
+
+# 4-14. PEP 8: Look through the original PEP 8 style guide at https://python.org/
+# dev/peps/pep-0008. You won’t use much of it now, but it might be interesting
+# to skim through it.
