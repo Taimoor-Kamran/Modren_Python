4048ebb70830bda90e363f81057145cf79730bd4
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-03-31 00:53:02 +0500

    added 231 commit

--- Class_10(functions)/Class_10.py
+++ Class_10(functions)/Class_10.py
@@ -1,19 +1,19 @@
 from typing import Callable
 
-# add: Callable[[int, int], int] = lambda x, y: x + y
-# result = add(10, 20) # result will be 30
-# print(result)
+add: Callable[[int, int], int] = lambda x, y: x + y
+result = add(10, 20) # result will be 30
+print(result)
 
-# from collections.abc import Iterator
+from collections.abc import Iterator
 
-# def my_range(start:int, end:int, stop: int=1) -> Iterator[int]:
-#     for i in range(start,end,stop):
-#         yield i
+def my_range(start:int, end:int, stop: int=1) -> Iterator[int]:
+    for i in range(start,end,stop):
+        yield i
 
-# a:Iterator[int] = my_range(1, 10)
-# print(next(a))
-# print(next(a))
-# print(type(a))
+a:Iterator[int] = my_range(1, 10)
+print(next(a))
+print(next(a))
+print(type(a))
 
 def decorator(func: Callable[[], None]) -> Callable[[], None]:
     def wrapper() -> None:
