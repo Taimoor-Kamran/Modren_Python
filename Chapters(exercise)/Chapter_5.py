1eb46a950a8b3ef27c33d281afd7cb47cdb8a1d0
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-03 16:20:14 +0500

    added 256 commit

--- Chapters(exercise)/Chapter_5.py
+++ Chapters(exercise)/Chapter_5.py
@@ -2,10 +2,32 @@
 # describing each test and your prediction for the results of each test. Your code
 # should look something like this:
 
+# • Look closely at your results, and make sure you understand why each line
+# evaluates to True or False.
+# • Create at least 10 tests. Have at least 5 tests evaluate to True and another
+# 5 tests evaluate to False.
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("Is car == 'audi'? I predict False.")
+print(car == 'audi')
+
 value = 10
-
-print("Is Value == 10 ? I predict True.")
+print("\nIs Value == 10 ? I predict True.")
 print(value == 10)
-
-print("\nIs Value == 20 ? I predict False.")
+print("Is Value == 20 ? I predict False.")
 print(value == 20)
+
+print("\nIs Value > 10 ? I predict True")
+print(value > 7)
+print("Is Value > 20 ? I predict False")
+print(value < 7)
+
+colors = ['red', 'blue', 'green']
+print("\nIs Color == 'red'? I predict True.")
+print('red' in colors)
+
+print("Is Color == 'yellow'? I predict False.")
+print('yellow' in colors)
+
