ece7d3bf3cf2a2b5038b8596ba1ae1b3a6799e50
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-03 16:42:44 +0500

    added 257 commit

--- Chapters(exercise)/Chapter_5.py
+++ Chapters(exercise)/Chapter_5.py
@@ -31,3 +31,59 @@ print('red' in colors)
 print("Is Color == 'yellow'? I predict False.")
 print('yellow' in colors)
 
+# 5-2. More Conditional Tests: You don’t have to limit the number of tests you cre-
+# ate to 10. If you want to try more comparisons, write more tests and add them
+
+# to conditional_tests.py. Have at least one True and one False result for each of
+# the following:
+
+# • Tests for equality and inequality with strings
+# • Tests using the lower() method
+# • Numerical tests involving equality and inequality, greater than and less
+# than, greater than or equal to, and less than or equal to
+# • Tests using the and keyword and the or keyword
+# • Test whether an item is in a list
+# • Test whether an item is not in a list
+
+# • Tests for equality and inequality with strings
+
+city = "karachi"
+
+print("\nIs city == karachi ? I predict True")
+print(city == "karachi")
+print("Is city == Lahore ? I predict False")
+print(city == "lahore")
+
+print("\nIs city != karachi ? I predict True")
+print(city != "lahore")
+print("Is city != Lahore ? I predict False")
+print(city != "karachi")
+
+# • Tests using the lower() method
+
+print("\nIs city.lower == karachi ? I predict True")
+print(city.lower() == "karachi")
+print("Is city == KARACHI ? I predict False")
+print(city.lower() == "KARACHI")
+
+print("\nIs city != karachi ? I predict True")
+print(city.lower() != "Karachi")
+print("Is city != karachi ? I predict False")
+print(city.lower() != "karachi")
+
+# • Numerical tests involving equality and inequality, greater than and less
+# than, greater than or equal to, and less than or equal to
+
+print("\nIs Value > 10 ? I predict True")
+print(value > 8)
+print("Is Value > 20 ? I predict False")
+print(value < 8)
+
+
+print("\nIs Value > 10 ? I predict True")
+print(value >= 8)
+print("Is Value > 20 ? I predict False")
+print(value <= 8)
+
+# • Tests using the and keyword and the or keyword
+
