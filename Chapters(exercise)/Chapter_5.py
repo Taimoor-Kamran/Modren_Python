9adf30f5fe37d6d43a977ae58053f7d094bfe68b
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-03 16:57:22 +0500

    added 261 commit

--- Chapters(exercise)/Chapter_5.py
+++ Chapters(exercise)/Chapter_5.py
@@ -115,3 +115,10 @@ print("\nIs watermelon in furits ? I predict True")
 print('watermelon' not in furits)
 print("Is apple in furits ? I predict False")
 print('apple' not in furits)
+
+# 5-3. Alien Colors #1: Imagine an alien was just shot down in a game. Create a
+# variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+# • Write an if statement to test whether the alien’s color is green. If it is, print
+# a message that the player just earned 5 points.
+# • Write one version of this program that passes the if test and another that
+# fails. (The version that fails will have no output.)
