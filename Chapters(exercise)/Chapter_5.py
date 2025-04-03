cdbbe93cec6140949d474a69981a9ffba97c94a5
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-04 04:24:44 +0500

    added 264 commit

--- Chapters(exercise)/Chapter_5.py
+++ Chapters(exercise)/Chapter_5.py
@@ -153,3 +153,35 @@ if(alien_color == "green"):
 else:
     print("the player just earned 10 points.")
 
+# 5-5. Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-elif-
+# else chain.
+
+# • If the alien is green, print a message that the player earned 5 points.
+# • If the alien is yellow, print a message that the player earned 10 points.
+# • If the alien is red, print a message that the player earned 15 points.
+# • Write three versions of this program, making sure each message is printed
+# for the appropriate color alien.
+
+alien_color = "green"
+
+if (alien_color == "red"):
+    print("the player earned 5 points.")
+elif (alien_color == "green"):
+    print("the player earned 10 points.")
+else:
+    print("the player earned 15 points.")
+
+# 5-6. Stages of Life: Write an if-elif-else chain that determines a person’s stage
+# of life. Set a value for the variable age, and then:
+# • If the person is less than 2 years old, print a message that the person is
+# a baby.
+# • If the person is at least 2 years old but less than 4, print a message that the
+# person is a toddler.
+# • If the person is at least 4 years old but less than 13, print a message that
+# the person is a kid.
+# • If the person is at least 13 years old but less than 20, print a message that
+# the person is a teenager.
+# • If the person is at least 20 years old but less than 65, print a message that
+# the person is an adult.
+# • If the person is age 65 or older, print a message that the person is an elder.
+
