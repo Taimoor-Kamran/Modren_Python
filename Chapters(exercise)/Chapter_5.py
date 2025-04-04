afccf68569a4806a51663fe82bb81bb3835fb9a8
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-04 16:39:45 +0500

    added 269 commit

--- Chapters(exercise)/Chapter_5.py
+++ Chapters(exercise)/Chapter_5.py
@@ -230,3 +230,29 @@ elif  "grapes" in favorite_fruits :
 # • Otherwise, print a generic greeting, such as Hello Jaden, thank you for
 # logging in again.
 
+usernames: list[str] = ["admin", "Yahya", "Ahemd", "Ali", "Ghasiq"]
+
+for user in usernames:
+    if user == "admin":
+        print("Hello admin, would you like to see a status report?")
+    else:
+        print(f"Hello {user.title()}, thank you for logging in again.")
+
+# 5-9. No Users: Add an if test to hello_admin.py to make sure the list of users is
+# not empty.
+# • If the list is empty, print the message We need to find some users!
+
+# • Remove all of the usernames from your list, and make sure the correct mes-
+# sage is printed.
+
+users = ["Admin", "Yahya", "Ahmed"]
+
+if users:
+    for user in users:
+        if user == "Admin":
+            print("Hello Admin, would like to see a status report?")
+        else:
+            print(f"Hello, {user.title()}, thank you for logging in again.")
+else:
+    print("We need to find some users")
+
