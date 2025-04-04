1bf7eb7fc071c9c6274cd36bf761b1181d643618
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-04 05:58:09 +0500

    added 268 commit

--- Chapters(exercise)/Chapter_5.py
+++ Chapters(exercise)/Chapter_5.py
@@ -221,3 +221,12 @@ elif  "orange" in favorite_fruits :
 elif  "grapes" in favorite_fruits :
     print("You really like grapes!")
 
+# 5-8. Hello Admin: Make a list of five or more usernames, including the name
+# 'admin'. Imagine you are writing code that will print a greeting to each user
+# after they log in to a website. Loop through the list, and print a greeting to
+# each user.
+# • If the username is 'admin', print a special greeting, such as Hello admin,
+# would you like to see a status report?
+# • Otherwise, print a generic greeting, such as Hello Jaden, thank you for
+# logging in again.
+
