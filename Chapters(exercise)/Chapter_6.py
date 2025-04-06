f3e9f49038508afa0fb19690f9060adaac7b4a41
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-06 22:42:35 +0500

    added 291 commit

--- Chapters(exercise)/Chapter_6.py
+++ Chapters(exercise)/Chapter_6.py
@@ -131,3 +131,10 @@ for person in people:
     print(f"Last Name: {person['last_name']}")
     print(f"Age: {person['age']}")
     print(f"City: {person['city']}")
+
+# 6-8. Pets: Make several dictionaries, where each dictionary represents a differ-
+# ent pet. In each dictionary, include the kind of animal and the owner’s name.
+
+# Store these dictionaries in a list called pets. Next, loop through your list and as
+# you do, print everything you know about each pet.
+
