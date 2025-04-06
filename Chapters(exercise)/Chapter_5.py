4399efa4047fba62b7b926510707fb24992156c9
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-06 12:38:53 +0500

    added 275 commit

--- Chapters(exercise)/Chapter_5.py
+++ Chapters(exercise)/Chapter_5.py
@@ -303,3 +303,21 @@ for number in numbers:
 # 5-12. Styling if Statements: Review the programs you wrote in this chapter, and
 # make sure you styled your conditional tests appropriately.
 
+x = 5
+
+if x == 5:
+    print("True") # (4 spaces is standard)
+
+if (x == 5): # Aviod unnecessary parentheses
+    print("True")
+
+
+# 5-13. Your Ideas: At this point, you’re a more capable programmer than you
+# were when you started this book. Now that you have a better sense of how
+# real-world situations are modeled in programs, you might be thinking of some
+# problems you could solve with your own programs. Record any new ideas you
+
+# have about problems you might want to solve as your programming skills con-
+# tinue to improve. Consider games you might want to write, datasets you might
+
+# want to explore, and web applications you’d like to create.
