13e35e1f6c6c98d5d2c22837416f0936005287b0
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-06 19:15:09 +0500

    added 281 commit

--- Chapters(exercise)/Chapter_6.py
+++ Chapters(exercise)/Chapter_6.py
@@ -20,3 +20,14 @@ favorite_numbers: dict[str, int] = {"Ali" : 7, "Ahmed" : 5, "Bilalwal": 4, "haro
 for name, f_number  in favorite_numbers.items():
     print(f"{name}'s favorite number is {f_number}")
 
+# 6-3. Glossary: A Python dictionary can be used to model an actual dictionary.
+# However, to avoid confusion, let’s call it a glossary.
+# • Think of five programming words you’ve learned about in the previous
+# chapters. Use these words as the keys in your glossary, and store their
+# meanings as values.
+# • Print each word and its meaning as neatly formatted output. You might
+# print the word followed by a colon and then its meaning, or print the word
+# on one line and then print its meaning indented on a second line. Use the
+# newline character (\n) to insert a blank line between each word-meaning
+# pair in your output.
+
