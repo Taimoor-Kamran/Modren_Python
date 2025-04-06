5d0403ff5c8b041024f90053538a438c29d0381e
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-06 19:40:53 +0500

    added 283 commit

--- Chapters(exercise)/Chapter_6.py
+++ Chapters(exercise)/Chapter_6.py
@@ -39,3 +39,10 @@ glossary: dict[str] = {"Variables": "A container to storing a data and value",
 
 for word, meaning in glossary.items():
     print(f"{word.title()}:\n {meaning}")
+
+# 6-4. Glossary 2: Now that you know how to loop through a dictionary, clean
+# up the code from Exercise 6-3 (page 99) by replacing your series of print()
+# calls with a loop that runs through the dictionary’s keys and values. When
+# you’re sure that your loop works, add five more Python terms to your glossary.
+# When you run your program again, these new words and meanings should
+# automatically be included in the output.
