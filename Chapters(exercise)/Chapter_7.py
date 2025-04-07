3cb344b7b61e410c16f484ccdd2f72b3cd59fc70
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-07 14:38:53 +0500

    added 305 commit

--- Chapters(exercise)/Chapter_7.py
+++ Chapters(exercise)/Chapter_7.py
@@ -40,3 +40,26 @@ while True:
         break
     else:
         print(f"I'll add {topping} to your pizza.")
+
+# 7-5. Movie Tickets: A movie theater charges different ticket prices depending on
+# a person’s age. If a person is under the age of 3, the ticket is free; if they are
+# between 3 and 12, the ticket is $10; and if they are over age 12, the ticket is
+# $15. Write a loop in which you ask users their age, and then tell them the cost
+# of their movie ticket.
+
+
+while True:
+    age_input = input("Let me know your age (or type 'quit' to exit): ")
+
+    if age_input.lower() == 'quit':
+        print("Thank you! Enjoy your movie.")
+        break
+
+    age = int(age_input)
+
+    if age < 3:
+        print("The ticket is free.")
+    elif age >= 3 and age <= 12:
+        print("The ticket is $10.")
+    else:
+        print("The ticket is $15.")
