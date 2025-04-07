432f1569e9c60990d994d311a60ed467b4775c74
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-07 13:39:13 +0500

    added 301 commit

--- Chapters(exercise)/Chapter_7.py
+++ Chapters(exercise)/Chapter_7.py
@@ -18,4 +18,5 @@ if dinner <= 8:
 else:
     print("their table is ready.")
 
-    
+# 7-3. Multiples of Ten: Ask the user for a number, and then report whether the
+# number is a multiple of 10 or not.
