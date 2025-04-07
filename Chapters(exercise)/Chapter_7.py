320a2ed006bd1e1d956b1de4f21138ea71bb8cd1
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-07 13:30:10 +0500

    added 299 commit

--- Chapters(exercise)/Chapter_7.py
+++ Chapters(exercise)/Chapter_7.py
@@ -6,3 +6,8 @@ rental_car: str = input("what kind of rental car would you like?")
 
 print(f"“Let me see if I can find you a {rental_car}")
 
+# 7-2. Restaurant Seating: Write a program that asks the user how many people
+
+# are in their dinner group. If the answer is more than eight, print a message say-
+# ing they’ll have to wait for a table. Otherwise, report that their table is ready.
+
