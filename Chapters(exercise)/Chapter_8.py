c01c7933c2fca190e4171a97f7800f517c5ce8e7
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-08 15:54:50 +0500

    added 322 commit

--- Chapters(exercise)/Chapter_8.py
+++ Chapters(exercise)/Chapter_8.py
@@ -32,3 +32,8 @@ make_shirt("Meduim", "Code like a pool.")
 make_shirt(size="Meduim", message="Code like a pool.")
 
 
+# 8-4. Large Shirts: Modify the make_shirt() function so that shirts are large
+# by default with a message that reads I love Python. Make a large shirt and a
+# medium shirt with the default message, and a shirt of any size with a different
+# message.
+
