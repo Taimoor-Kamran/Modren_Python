9dd4d8e69fea0e0e1ea7ef53c5e55a6baa8204bd
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-08 13:56:20 +0500

    added 312 commit

--- Chapters(exercise)/Chapter_7.py
+++ Chapters(exercise)/Chapter_7.py
@@ -146,3 +146,10 @@ for sandwich_order in sandwich_orders:
 print("\nAll sandwiches made:")
 for sandwich in finished_sandwiches:
     print(f"- {sandwich}")
+
+# 7-9. No Pastrami: Using the list sandwich_orders from Exercise 7-8, make sure
+# the sandwich 'pastrami' appears in the list at least three times. Add code
+# near the beginning of your program to print a message saying the deli has
+# run out of pastrami, and then use a while loop to remove all occurrences of
+# 'pastrami' from sandwich_orders. Make sure no pastrami sandwiches end up
+# in finished_sandwiches.
