0bf6d72e0689529a9a4287441e11b577fb283b33
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-08 16:01:43 +0500

    added 324 commut

--- Chapters(exercise)/Chapter_8.py
+++ Chapters(exercise)/Chapter_8.py
@@ -45,3 +45,11 @@ make_shirt()
 make_shirt(size="Medium")
 
 make_shirt(size="Small", message="Never give up.")
+
+# 8-5. Cities: Write a function called describe_city() that accepts the name of
+# a city and its country. The function should print a simple sentence, such as
+# Reykjavik is in Iceland. Give the parameter for the country a default value.
+# Call your function for three different cities, at least one of which is not in the
+# default country.
+
+
