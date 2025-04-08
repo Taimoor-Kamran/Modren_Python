8ae2c4577341a712ea3b5c6ba3d1c55e86434620
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-08 16:08:30 +0500

    added 326 commit

--- Chapters(exercise)/Chapter_8.py
+++ Chapters(exercise)/Chapter_8.py
@@ -59,3 +59,9 @@ describe_city("Texas")
 describe_city("Karachi", "Pakistan")
 describe_city("Berlin", "Germany")
 
+# 8-6. City Names: Write a function called city_country() that takes in the name
+# of a city and its country. The function should return a string formatted like this:
+# "Santiago, Chile"
+# Call your function with at least three city-country pairs, and print the values
+# that are returned.
+
