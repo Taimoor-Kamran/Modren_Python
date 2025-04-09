46981e6e8147612c9e43a70134f32a847c7af586
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-09 17:39:33 +0500

    added 380 commit

--- Chapters(exercise)/Chapter_8.py
+++ Chapters(exercise)/Chapter_8.py
@@ -197,3 +197,10 @@ send_message(text_message[:], sent_messages)
 
 print(f"\nOrignal message list ('should be empty now:'), {text_message}")
 print(f"\nSent message list , {sent_messages}")
+
+# 8-12. Sandwiches: Write a function that accepts a list of items a person wants
+# on a sandwich. The function should have one parameter that collects as many
+
+# items as the function call provides, and it should print a summary of the sand-
+# wich that’s being ordered. Call the function three times, using a different num-
+# ber of arguments each time.
