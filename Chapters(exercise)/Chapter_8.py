1c32efade65ed2eb232e3f42e6e1d99f2f844ab8
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-09 17:51:05 +0500

    added 382 commit

--- Chapters(exercise)/Chapter_8.py
+++ Chapters(exercise)/Chapter_8.py
@@ -215,3 +215,7 @@ def sandwich(*items):
 sandwich('cheese', 'tomato', 'lettuce')
 sandwich('chicken', 'mayo')
 sandwich('beef', 'onion', 'mustard', 'pickles')
+
+# 8-13. User Profile: Start with a copy of user_profile.py from page 148. Build a
+# profile of yourself by calling build_profile(), using your first and last names
+# and three other key-value pairs that describe you.
