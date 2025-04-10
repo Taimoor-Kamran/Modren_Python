56632758e30e14ce0d048a2348bf7b82ffda3159
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-10 18:19:08 +0500

    added 385 commit

--- Chapters(exercise)/Chapter_8.py
+++ Chapters(exercise)/Chapter_8.py
@@ -228,3 +228,7 @@ def build_profile(first_name, last_name, **userinfo):
         profile[key] = value
     return profile
 
+# 8-4. Large Shirts: Modify the make_shirt() function so that shirts are large
+# by default with a message that reads I love Python. Make a large shirt and a
+# medium shirt with the default message, and a shirt of any size with a different
+# message.
