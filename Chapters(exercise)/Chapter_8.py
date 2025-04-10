4a04c5ef3207186332e48ca1d2364d5f956da7cc
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-10 18:21:59 +0500

    added 392 commit

--- Chapters(exercise)/Chapter_8.py
+++ Chapters(exercise)/Chapter_8.py
@@ -264,3 +264,9 @@ def build_profile(first_name, last_name, **userinfo):
 
 # 8-9. Messages: Make a list containing a series of short text messages. Pass the
 # list to a function called show_messages(), which prints each text message.
+
+# 8-10. Sending Messages: Start with a copy of your program from Exercise 8-9.
+# Write a function called send_messages() that prints each text message and
+# moves each message to a new list called sent_messages as it’s printed. After
+# calling the function, print both of your lists to make sure the messages were
+# moved correctly.
