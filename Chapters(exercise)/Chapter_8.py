58ab51d694d86865ea2dc7d10d98816bcf5f89dc
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-10 18:23:24 +0500

    added 395 commit

--- Chapters(exercise)/Chapter_8.py
+++ Chapters(exercise)/Chapter_8.py
@@ -282,3 +282,7 @@ def build_profile(first_name, last_name, **userinfo):
 # wich that’s being ordered. Call the function three times, using a different num-
 # ber of arguments each time.
 
+# 8-13. User Profile: Start with a copy of user_profile.py from page 148. Build a
+# profile of yourself by calling build_profile(), using your first and last names
+# and three other key-value pairs that describe you.
+
