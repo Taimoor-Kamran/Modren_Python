1ad83f8d516a880b27f180aace20c10d5af1b737
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-10 18:20:29 +0500

    added 388 commit

--- Chapters(exercise)/Chapter_8.py
+++ Chapters(exercise)/Chapter_8.py
@@ -246,3 +246,10 @@ def build_profile(first_name, last_name, **userinfo):
 # Call your function with at least three city-country pairs, and print the values
 # that are returned.
 
+# 8-7. Album: Write a function called make_album() that builds a dictionary
+# describing a music album. The function should take in an artist name and an
+# album title, and it should return a dictionary containing these two pieces of
+# information. Use the function to make three dictionaries representing different
+# albums. Print each return value to show that the dictionaries are storing the
+# album information correctly.
+
