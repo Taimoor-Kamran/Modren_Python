f4d0a638326be11905d1482d76cb810ba771f9ef
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-10 18:26:17 +0500

    added 402 commit

--- Chapters(exercise)/Chapter_9.py
+++ Chapters(exercise)/Chapter_9.py
@@ -12,3 +12,12 @@
 # different instances from the class, and call describe_restaurant() for each
 # instance.
 
+# 9-3. Users: Make a class called User. Create two attributes called first_name
+# and last_name, and then create several other attributes that are typically stored
+# in a user profile. Make a method called describe_user() that prints a summary
+# of the user’s information. Make another method called greet_user() that prints
+# a personalized greeting to the user.
+
+# Create several instances representing different users, and call both meth-
+# ods for each user.
+
