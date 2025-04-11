da800a82b3de0709b7eab662a8117222f92a6675
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-11 17:38:42 +0500

    added 410 commit

---
+++ Chapters(exercise)/Chapter_8(exercise)/printing_models.py
@@ -0,0 +1,2 @@
+from printing_function import print_models, show_completed_models
+
--- Chapters(exercise)/Chapter_8.py
+++ Chapters(exercise)/Chapter_8.py
@@ -255,9 +255,13 @@ def make_info(manufacture , model, **car_info):
 my_car = make_info("subaru", "outback", color="blue", two_package=True)
 
 print(my_car)
+
 # 8-15. Printing Models: Put the functions for the example printing_models.py in a
 # separate file called printing_functions.py. Write an import statement at the top
 # of printing_models.py, and modify the file to use the imported functions.
+
+
+
 # 8-16. Imports: Using a program you wrote that has one function in it, store that
 # function in a separate file. Import the function into your main program file, and
 # call the function using each of these approaches:
@@ -266,6 +270,8 @@ print(my_car)
 # from module_name import function_name as fn
 # import module_name as mn
 # from module_name import *
+
+
 
 # 8-17. Styling Functions: Choose any three programs you wrote for this chapter,
 # and make sure they follow the styling guidelines described in this section.
