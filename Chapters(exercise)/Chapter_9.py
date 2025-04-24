1afdac2d13c93d7fef3a1f909b0bd46a0cd7dfd9
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-24 17:23:42 +0500

    added 556

--- Chapters(exercise)/Chapter_9.py
+++ Chapters(exercise)/Chapter_9.py
@@ -2,21 +2,19 @@
 # Restaurant should store two attributes: a restaurant_name and a cuisine_type.
 # Make a method called describe_restaurant() that prints these two pieces of
 
-class Restaurant:
-    def __init__(self, restaurant_name, cuisine_type):
-        self.restaurant_name = restaurant_name
-        self.cuisine_type = cuisine_type
-
-        def describe_restaurant(self):
-            print(f"R")
-
-
-
 # information, and a method called open_restaurant() that prints a message indi-
 # cating that the restaurant is open.
 
 # Make an instance called restaurant from your class. Print the two attri-
 # butes individually, and then call both methods.
+
+class Restaurant:
+    def __init__(self, restaurant_name: str, cuisine_type: str):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(f"")
 
 # 9-2. Three Restaurants: Start with your class from Exercise 9-1. Create three
 # different instances from the class, and call describe_restaurant() for each
