77bd6e0073ea3b631e4103f5c2e5e7080d5623cf
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-24 17:15:22 +0500

    added 555 commit

--- Chapters(exercise)/Chapter_9.py
+++ Chapters(exercise)/Chapter_9.py
@@ -2,6 +2,14 @@
 # Restaurant should store two attributes: a restaurant_name and a cuisine_type.
 # Make a method called describe_restaurant() that prints these two pieces of
 
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+        def describe_restaurant(self):
+            print(f"R")
+
 
 
 # information, and a method called open_restaurant() that prints a message indi-
