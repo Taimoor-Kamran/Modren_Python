069a37ab301118ce8a3d9714d25c6908235ecbb1
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-25 16:37:33 +0500

    added 582 commit

--- Chapters(exercise)/Chapter_9.py
+++ Chapters(exercise)/Chapter_9.py
@@ -94,6 +94,17 @@ user3.greet_user()
 # instance called restaurant from this class. Print the number of customers the
 # restaurant has served, and then change this value and print it again.
 
+
+# Add a method called set_number_served() that lets you set the number of
+# customers that have been served. Call this method with a new number and print
+# the value again.
+
+# Add a method called increment_number_served() that lets you increment
+# the number of customers who’ve been served. Call this method with any number
+# you like that could represent how many customers were served in, say, a day of
+# business.
+
+
 class Restaurent:
     def __init__(self, restaurent_name:str, cuisine_type:str):
         self.restaurent_name = restaurent_name
@@ -106,6 +117,14 @@ class Restaurent:
 
     def open_restaurant(self):
         print(f"{self.restaurent_name} is now open!")
+
+    def set_number_served(self, number):
+        self.number_served = number
+
+    def increment_number_served(self, additional_customers):
+        self.number_served += additional_customers
+
+
 restaurant = Restaurent("Tasty Bites", "Italian")
 
 print(f"Customers served: {restaurant.number_served}")
@@ -113,12 +132,9 @@ print(f"Customers served: {restaurant.number_served}")
 restaurant.number_served = 25
 print(f"Updated customers served: {restaurant.number_served}")
 
-# Add a method called set_number_served() that lets you set the number of
-# customers that have been served. Call this method with a new number and print
-# the value again.
 
-# Add a method called increment_number_served() that lets you increment
-# the number of customers who’ve been served. Call this method with any number
-# you like that could represent how many customers were served in, say, a day of
-# business.
+restaurant.set_number_served(100)
+print(f"Set number served: {restaurant.number_served}")
 
+restaurant.increment_number_served(50)
+print(f"After increment, total customers served: {restaurant.restaurent_name}")
