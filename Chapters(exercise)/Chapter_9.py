86a6a2db0f87c6492b9edf5ae4b674b9c35ef6cd
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-05-26 05:49:02 +0500

    added 693 commit

--- Chapters(exercise)/Chapter_9.py
+++ Chapters(exercise)/Chapter_9.py
@@ -379,13 +379,13 @@ user_admin.privileges.show_privileges()
 
 # run main.py
 
-
 # 9-13. Dice: Make a class Die with one attribute called sides, which has a
 
 # default value of 6. Write a method called roll_die() that prints a random num-
 # ber between 1 and the number of sides the die has. Make a 6-sided die and
 
 # roll it 10 times.
+# Make a 10-sided die and a 20-sided die. Roll each die 10 times..
 
 import random
 
